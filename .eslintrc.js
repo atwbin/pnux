8a62efb0053f4d70bde0dfcf73f2562c44aadca0
wangbin <wangbin@kaolakuaishou.com>
2020-08-21 16:56:12 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/prettier',
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-expressions': 'off',
+    'no-console': 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# pnuxt
+
+nuxt 移动端 模版，
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run export // 生成 dist/
+$ npm run serve // 运行服务器
+
+// npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+
+## TODO
+
+- [ ] Less
+- [x] normalize.css
+
